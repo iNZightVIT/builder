556d449716c57a18f59d02d52e6d945764403885
Tom Elliott <tomelliottnz@gmail.com>
2020-09-08 20:35:55 +1200

    List files and cache R

--- build.R
+++ build.R
@@ -75,5 +75,7 @@ if (any(pkgs$replace)) {
         # Move new binaries into place
         system(sprintf("mv *.zip %s", dir))
         tools::write_PACKAGES(dir)
+
+        system("ls -lR bin")
     }
 }
