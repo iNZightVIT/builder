d6809eef0fea98531334668e4883b56c5d246b74
Tom Elliott <tomelliottnz@gmail.com>
2020-09-08 15:15:42 +1200

    Get package list

--- build.R
+++ build.R
@@ -1,12 +1,21 @@
 # build packages
 
 os <- Sys.getenv("OS_TYPE")
-
 sources <- os == "Linux"
+rv <- paste(strsplit(as.character(getRversion()), "\\.")[[1]][1:2],
+    collapse = ".")
+
+# current versions:
+dir <- ifelse(sources,
+    "src/contrib",
+    sprintf("windows/contrib/%s", rv)
+)
+pkgs <- read.dcf(file.path(dir, "PACKAGES"))
+print(pkgs)
 
 if (sources) {
-    print("Building sources")
-    system("ls -lR src")
+    message(" === Building sources ===")
+
 } else {
     print("Building binaries")
 }
