4a2392b513ba73e0e0b6095042c72cc849cc7166
Tom Elliott <tomelliottnz@gmail.com>
2020-09-08 15:35:06 +1200

    Give building sources a go ..

--- build.R
+++ build.R
@@ -10,12 +10,43 @@ dir <- ifelse(sources,
     "src/contrib",
     sprintf("windows/contrib/%s", rv)
 )
-pkgs <- read.dcf(file.path(dir, "PACKAGES"))
-print(pkgs)
+current_pkgs <- read.dcf(file.path(dir, "PACKAGES"))
+current_pkgs <- current_pkgs[, c("Package", "Version")]
 
-if (sources) {
-    message(" === Building sources ===")
+# newest versions:
+new_pkgs <- do.call(
+    rbind,
+    lapply(list.files("library", full.names = TRUE),
+        function(f) {
+            desc <- read.dcf(file.path(f, "DESCRIPTION"))
+            desc[, c("Package", "Version")]
+        }
+    )
+)
 
-} else {
-    print("Building binaries")
+# compare
+pkgs <- merge(current_pkgs, new_pkgs,
+    by = "Package",
+    suffix = c("_cur", "_new")
+)
+pkgs$replace <- numeric_version(pkgs$Version_new) > numeric_version(pkgs$Version_cur)
+
+if (any(pkgs$replace)) {
+    # the packages that need updating are:
+    replace_pkgs <- as.character(pkgs$Package[pkgs$replace])
+    if (sources) {
+        message(" === Building sources ===")
+        for (pkg in replace_pkgs)
+            system(
+                sprintf("R CMD build --no-build-vignettes %s",
+                    file.path("library", pkg)
+                )
+            )
+        system(sprintf("mv *.tar.gz %s", dir))
+        tools::write_PACKAGES(dir)
+    } else {
+        print("Building binaries")
+    }
 }
+
+system(sprintf("ls -lR %s", dir))
