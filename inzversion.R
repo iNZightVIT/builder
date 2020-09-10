b849350dd089bd670ef3c9cc7ad80fc2a3e89dcf
Tom Elliott <tomelliottnz@gmail.com>
2020-09-10 15:17:42 +1200

    Better? [skip repo]

--- inzversion.R
+++ inzversion.R
@@ -40,16 +40,16 @@ if (length(vers) == 1 && is.na(vers)) {
     )
     comp$update <-
         package_version(comp$version.cur) < package_version(comp$version.new)
-    if (any(!is.na(comp$update)) || any(comp$update)) {
+    if (any(is.na(comp$update)) || any(comp$update)) {
         # updates are required
-        VERSION <- pkgs["iNZight", "version"]
-        if (!comp["iNZight", "update"]) {
+        VERSION <- pkgs[pkgs$package == "iNZight", "version"]
+        if (!comp[pkgs$package == "iNZight", "update"]) {
             # need to add 1 to patch version since iNZight package not updated
             # (but others are):
             v <- package_version(VERSION)
             p <- v[[1, 4]]
             if (is.na(p)) p <- 0
-            VERSION <- paste(as.character(v[1, 1:3]), p, sep = "-")
+            VERSION <- paste(as.character(v[1, 1:3]), p + 1, sep = "-")
         }
     } else {
         # no update necessary
