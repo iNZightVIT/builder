a14f02d0db3b7a88bd9ef9f788b10c108a47f82f
Tom Elliott <tomelliottnz@gmail.com>
2020-09-14 09:09:10 +1200

    Pass extension so win zips delete too

--- clean.R
+++ clean.R
@@ -1,9 +1,11 @@
 # clean old packages
 ca <- commandArgs(TRUE)
+dir <- ca[1]
+ext <- ca[2]
 
 pkgs <- list.files("library")
-src <- gsub("_.+", "", list.files(ca[1], ".tar.gz"))
+src <- gsub("_.+", "", list.files(dir, ext))
 del <- src[!src %in% pkgs]
 
 for (pkg in del)
-    unlink(paste0(file.path(ca, pkg), "*.tar.gz"))
+    unlink(paste0(file.path(dir, pkg), "_*", ext))
