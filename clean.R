8816350db5c5a3562c59ed04359ba2abb126216d
Tom Elliott <tomelliottnz@gmail.com>
2020-09-14 08:53:32 +1200

    Fix clean script

--- clean.R
+++ clean.R
@@ -2,7 +2,8 @@
 ca <- commandArgs(TRUE)
 
 pkgs <- list.files("library")
-src <- list.files(ca[1])
+src <- gsub("_.+", "", list.files(ca[1], ".tar.gz"))
 del <- src[!src %in% pkgs]
 
-for (pkg in del) unlink(file.path(ca, pkg))
+for (pkg in del)
+    unlink(paste0(file.path(ca, pkg), "*.tar.gz"))
