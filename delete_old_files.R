0d4cb1c25426dbc81b22c79cdf6d05a8a1750344
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-04-16 08:50:03 +1200

    fix typo

--- delete_old_files.R
+++ delete_old_files.R
@@ -1,5 +1,8 @@
 ca <- commandArgs(trailingOnly = TRUE)
 
+# Delete all nightly builds based on earlier versions (< x.y.z)
+# And only keep the 7 most recent
+
 delete_files <- function(ca) {
     if (length(ca) == 0) return()
     file_list <- ca[1]
