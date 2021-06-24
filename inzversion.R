02960477771d04c406045e878c936778359d0b81
Tom Elliott <tomelliottnz@gmail.com>
2021-06-24 16:30:44 +1200

    set version with 4 components

--- inzversion.R
+++ inzversion.R
@@ -1,64 +1,10 @@
 # Compute the version of iNZight to use
-f <- "downloads/windows_versions"
-vfile <- "downloads/VERSION"
-if (file.exists(f)) {
-    vers <- read.dcf(f)
-} else {
-    vers <- NA
-}
+url <- "https://r.docker.stat.auckland.ac.nz/downloads/VERSION"
+v <- package_version(readLines(url))
+p <- v[[1L, 4L]]
+if (is.na(p)) p <- 0
+VERSION <- paste(as.character(v[1L, 1:3]), p, sep = ".")
 
-# installer depends on these packages:
-pkgs <- c(
-    "iNZightTools",
-    "iNZightMR",
-    "iNZightTS",
-    "iNZightPlots",
-    "iNZightRegression",
-    "FutureLearnData",
-    "iNZight",
-    "iNZightModules",
-    "vit",
-    "iNZightUpdate"
-)
-
-new <- sapply(pkgs, function(p) as.character(packageVersion(p)))
-
-pkgs <- data.frame(package = pkgs, version = new)
-
-if (length(vers) == 1L && is.na(vers)) {
-    # nothing to compare to
-    VERSION <- pkgs[pkgs$package == "iNZight", "version"]
-} else {
-    # compare versions:
-    comp <- merge(pkgs, vers,
-        by = "package",
-        suffixes = c(".new", ".cur"),
-        all.x = TRUE
-    )
-    # any NA go to zero
-    comp$version.new <- ifelse(is.na(comp$version.new), "0.0.1", comp$version.new)
-    comp$version.cur <- ifelse(is.na(comp$version.cur), "0.0.1", comp$version.cur)
-    comp$update <-
-        package_version(comp$version.cur) < package_version(comp$version.new)
-    if (any(is.na(comp$update)) || any(comp$update)) {
-        # updates are required
-        VERSION <- pkgs[pkgs$package == "iNZight", "version"]
-        if (!comp[comp$package == "iNZight", "update"]) {
-            if (file.exists(vfile)) {
-                v <- package_version(scan(vfile, character(), quiet = TRUE))
-            } else {
-                v <- package_version(VERSION)
-            }
-            p <- v[[1L, 4L]]
-            if (is.na(p)) p <- 0
-            VERSION <- paste(as.character(v[1L, 1:3]), p + 1, sep = ".")
-        }
-    } else {
-        # no update necessary
-        message("No changes - skipping installer build.")
-        quit(status = 1)
-    }
-}
 message(VERSION)
 # this will be uploaded with the installer (and not if build fails!)
 # write.dcf(pkgs, file = file.path("downloads", "windows_versions"))
