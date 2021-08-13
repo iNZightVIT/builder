73755fa6b17702ce53cddd6ea2752b743aee79a0
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-08-13 14:00:20 +1200

    fix for install dempkgs

--- install.R
+++ install.R
@@ -17,8 +17,11 @@ pkgs <- c(
 )
 
 OS <- Sys.getenv("OS_TYPE")
-if (OS == "Windows" && !requireNamespace('utf8', quietly = TRUE)) {
-    install.packages("utf8", repos = "https://cran.rstudio.com")
+if (OS == "Windows") {
+    if (!requireNamespace('utf8', quietly = TRUE))
+        install.packages("utf8", repos = "https://cran.rstudio.com")
+    dempkgs <- pkgs[grepl("^dem", pkgs)]
+    pkgs <- pkgs[!grepl("^dem", pkgs)]
 }
 if (OS == "macOS") {
     pkgs <- pkgs[!pkgs %in% c("iNZight", "iNZightModules", "vit", "iNZightUpdate")]
@@ -38,4 +41,19 @@ for (pkg in pkgs) {
         file.path("library", pkg),
         INSTALL_opts = "--no-multiarch"
     )
+}
+if (OS == "Windows") {
+    ap <- utils::available.packages()
+    dem_ap <- dempkgs %in% row.names(ap)
+    if (any(dem_ap)) {
+        # install available packages from repo
+        utils::install.packages(dempkgs[dem_ap])
+    }
+    if (any(!dem_ap)) {
+        # install unavailable packages from local
+        remotes::install_local(
+            file.path("library", dempkgs[!dem_ap]),
+            INSTALL_opts = "--no-multiarch"
+        )
+    }
 }
