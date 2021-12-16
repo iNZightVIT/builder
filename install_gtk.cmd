2296f2412c8f7e2ed19f0a754183e5b469c16ee8
Tom Elliott <tomelliottnz@gmail.com>
2021-12-17 09:46:32 +1300

    Install GTK binaries before RGtk2

--- install_gtk.cmd
+++ install_gtk.cmd
@@ -1,11 +1,18 @@
 if exist D:\a\_temp\Library\RGtk2\gtk\NUL exit 0
 
-echo "Installing RGtk2 ..."
-Rscript -e "install.packages('RGtk2', repos = 'https://cloud.r-project.org', type = 'win.binary')"
+@REM echo "Installing RGtk2 ..."
+@REM Rscript -e "install.packages('RGtk2', repos = 'https://cloud.r-project.org', type = 'win.binary')"
 
 echo "Installing gtk ..."
 curl -L -o gtk.zip http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip
+md D:\a\_temp\GTK
 md gtk
+7z x gtk.zip -oD:\a\_temp\GTK > nul
 7z x gtk.zip -ogtk > nul
 del gtk.zip
+
+echo "Install RGtk2 ..."
+R CMD INSTALL library/RGtk2
+
+echo "Copying GTK binaries to RGtk2 package ..."
 mv gtk D:\a\_temp\Library\RGtk2\
