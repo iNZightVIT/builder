ee6f87110f7a4f0015b05520a91a8d95df218dfc
Tom Elliott <tomelliottnz@gmail.com>
2022-01-11 09:13:09 +1300

    install RGtk2 binary from repo

--- install_gtk.cmd
+++ install_gtk.cmd
@@ -1,3 +1,5 @@
+if exist D:\a\_temp\Library\RGtk2\gtk\NUL exit 0
+
 echo "Installing gtk ..."
 curl -L -o gtk.zip http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-bundle_2.22.1-20101229_win64.zip
 md D:\a\_temp\GTK
@@ -6,9 +8,8 @@ md gtk
 7z x gtk.zip -ogtk > nul
 del gtk.zip
 
-if exist D:\a\_temp\Library\RGtk2\gtk\NUL exit 0
 echo "Install RGtk2 ..."
-R CMD INSTALL library/RGtk2
+Rscript -e "Sys.setenv(GTK_PATH = file.path(getwd(), 'gtk')); install.packages('RGtk2', repos = 'https://r.docker.stat.auckland.ac.nz')"
 
 echo "Copying GTK binaries to RGtk2 package ..."
 mv gtk D:\a\_temp\Library\RGtk2\
