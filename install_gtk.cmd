87ca12ade8613fa0c78bc38bb8b0f35ee0f7162f
Tom Elliott <tomelliottnz@gmail.com>
2022-04-28 21:52:59 +1200

    cairoDevice from patched tmelliott fork

--- install_gtk.cmd
+++ install_gtk.cmd
@@ -19,7 +19,7 @@ R CMD build RGtk2-master\RGtk2
 R CMD INSTALL RGtk2_2.20.40.tar.gz
 
 echo "Install cairoDevice ..."
-curl -L -o cairoDevice.zip https://github.com/lawremi/cairoDevice/archive/refs/heads/master.zip
+curl -L -o cairoDevice.zip https://github.com/tmelliott/cairoDevice/archive/refs/heads/master.zip
 7z x cairoDevice.zip > nul
 del cairoDevice.zip
 R CMD build cairoDevice-master
