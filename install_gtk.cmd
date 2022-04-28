1c91471b9019f2eea61e1998db79de41ce87f7b2
Tom Elliott <tomelliottnz@gmail.com>
2022-04-28 13:08:55 +1200

    Try using .Renviron file

--- install_gtk.cmd
+++ install_gtk.cmd
@@ -13,7 +13,7 @@ curl -L -o RGtk2.zip https://github.com/lawremi/RGtk2/archive/refs/heads/master.
 7z x RGtk2.zip > nul
 del RGtk2.zip
 set "GTK_PATH=%cd%\gtk\x64"
-set "TMP=D:\a\_temp\Rtmp"
+Rscript -e "write('TMP = D:\a\_temp\Rtmp', file = '.Renviron')"
 R CMD build RGtk2-master\RGtk2
 R CMD INSTALL RGtk2_2.20.40.tar.gz
 Rscript -e "Sys.setenv(GTK_PATH = file.path(getwd(), 'gtk', 'x64')); install.packages('./RGtk2-master/RGtk2', repos = NULL, type = 'source')"
