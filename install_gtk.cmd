15f5f785fcdb83c090905f28a90dd9fd3ccfa550
Tom Elliott <tomelliottnz@gmail.com>
2022-04-28 15:50:39 +1200

    revert

--- install_gtk.cmd
+++ install_gtk.cmd
@@ -5,18 +5,16 @@ curl -L -o gtk.zip http://ftp.gnome.org/pub/gnome/binaries/win64/gtk+/2.22/gtk+-
 md D:\a\_temp\GTK
 7z x gtk.zip -ox64 > nul
 del gtk.zip
-mv x64\ D:\a\_temp\GTK
-dir D:\a\_temp\GTK
+mv x64 D:\a\_temp\GTK
 
 echo "Install RGtk2 ..."
 curl -L -o RGtk2.zip https://github.com/lawremi/RGtk2/archive/refs/heads/master.zip
 7z x RGtk2.zip > nul
 del RGtk2.zip
-
 rm RGtk2-master\RGtk2\src\RGtk2
 ln -s ..\inst\include\RGtk2 RGtk2-master\RGtk2\src\RGtk2
 R CMD build RGtk2-master\RGtk2
 R CMD INSTALL RGtk2_2.20.40.tar.gz
 
 echo "Copying GTK binaries to RGtk2 package ..."
-@REM mv gtk D:\a\_temp\Library\RGtk2\
+mv gtk D:\a\_temp\Library\RGtk2\
