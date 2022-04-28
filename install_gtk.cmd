ab82b420a43c458f4512a1f3881a3e706e7f387c
Tom Elliott <tomelliottnz@gmail.com>
2022-04-28 13:42:09 +1200

    Paths spec moved to GH file

--- install_gtk.cmd
+++ install_gtk.cmd
@@ -6,15 +6,15 @@ md D:\a\_temp\GTK
 md gtk
 7z x gtk.zip -ox64 > nul
 del gtk.zip
-mv x64 gtk\x64
+md D:\a\_temp\GTK
+mv x64 D:\a\_temp\GTK\x64
 
 echo "Install RGtk2 ..."
 curl -L -o RGtk2.zip https://github.com/lawremi/RGtk2/archive/refs/heads/master.zip
 7z x RGtk2.zip > nul
 del RGtk2.zip
-set "GTK_PATH=%cd%\gtk\x64"
+
 md D:\a\_temp\Rtmp
-set "TEMPDIR=D:\a\_temp\Rtmp"
 Rscript -e "print(tempdir())"
 R CMD build RGtk2-master\RGtk2
 R CMD INSTALL RGtk2_2.20.40.tar.gz
