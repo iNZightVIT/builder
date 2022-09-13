777c9641f4eef168766e1dca7615d860a94d7a84
Tom Elliott <tomelliottnz@gmail.com>
2022-09-13 14:01:40 +1200

    install Matrix from source as (currently) unavailable on CRAN for R 4.1

--- install_nightly_guinz.R
+++ install_nightly_guinz.R
@@ -27,6 +27,10 @@ pkgs <- c(
 curr <- as.character(installed.packages()[, "Package"])
 install.packages(c("httr", "lubridate"))
 
+if (packageVersion("Matrix") < numeric_version("5.0")) {
+    install.packages("Matrix", type = "source")
+}
+
 print(curr)
 
 # download all
@@ -36,18 +40,25 @@ sapply(pkgs, function(pkg) {
 
     # if there is a release- branch NEWER than the dev branch,
     # use that instead:
-    x <- httr::GET(sprintf('https://api.github.com/repos/%s/%s/branches', pkg[1], pkg[2]))
+    x <- httr::GET(sprintf("https://api.github.com/repos/%s/%s/branches", pkg[1], pkg[2]))
     branches <- httr::content(x)
-    if (!is.null(branches$message)) return()
+    if (!is.null(branches$message)) {
+        return()
+    }
 
     names(branches) <- sapply(branches, function(z) z$name)
     releaseBranches <- branches[sapply(names(branches), function(z) grepl("release", z))]
     branch <- "feature/guinz"
     if (is.null(branches[[branch]])) {
-        if ("dev" %in% names(branches)) branch <- "dev"
-        else if ("develop" %in% names(branches)) branch <- "develop"
-        else if ("main" %in% names(branches)) branch <- "main"
-        else branch <- "master"
+        if ("dev" %in% names(branches)) {
+            branch <- "dev"
+        } else if ("develop" %in% names(branches)) {
+            branch <- "develop"
+        } else if ("main" %in% names(branches)) {
+            branch <- "main"
+        } else {
+            branch <- "master"
+        }
     }
     if (length(releaseBranches)) {
         devBranch <- branches[[branch]]
@@ -80,7 +91,9 @@ pkgs <- gsub(".*/", "", pkgs)
 
 # query and install dependencies
 deps <- sapply(pkgs, function(pkg) {
-    if (!file.exists(sprintf("%s.zip", pkg))) return()
+    if (!file.exists(sprintf("%s.zip", pkg))) {
+        return()
+    }
     d <- gsub("/$", "", utils::unzip(sprintf("%s.zip", pkg), list = TRUE)[1, "Name"])
     on.exit(unlink(d, recursive = TRUE, force = TRUE))
     desc <- utils::unzip(
