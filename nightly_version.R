d325d7091339943cc1e1c6cf77e1786d75d1ff9d
Tom Elliott <tomelliottnz@gmail.com>
2023-01-18 10:07:36 +1300

    make version script more robust

--- nightly_version.R
+++ nightly_version.R
@@ -1,5 +1,12 @@
 v <- strsplit(as.character(packageVersion("iNZight")), "\\.")[[1]]
-if (length(v) == 3L || v[4] == "9000") {
+if (length(v) == 3L) {
     v[4] <- format(Sys.time(), "%Y%m%d", tz = "Pacific/Auckland")
+} else if (length(v) == 4L) {
+    v[4] <- ifelse(v[4] == "9000",
+        format(Sys.time(), "%Y%m%d", tz = "Pacific/Auckland"),
+        v[4]
+    )
+} else {
+    stop("Invalid version number")
 }
 cat(paste(v, collapse = "."))
