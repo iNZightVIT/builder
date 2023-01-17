cac920beebbd81491411c91d704106ff8fb30cb3
Tom Elliott <tomelliottnz@gmail.com>
2023-01-18 10:23:52 +1300

    allow shorter versions

--- nightly_version.R
+++ nightly_version.R
@@ -1,5 +1,7 @@
 v <- strsplit(as.character(packageVersion("iNZight")), "\\.")[[1]]
-if (length(v) == 3L) {
+if (length(v) < 3L) {
+    v <- c(v, rep("0", 3L - length(v)))
+} else if (length(v) == 3L) {
     v[4] <- format(Sys.time(), "%Y%m%d", tz = "Pacific/Auckland")
 } else if (length(v) == 4L) {
     v[4] <- ifelse(v[4] == "9000",
