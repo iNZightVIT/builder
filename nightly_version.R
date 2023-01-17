49b744d20a713f8a8ba320d42a680a3bbccd4e52
Tom Elliott <tomelliottnz@gmail.com>
2023-01-18 09:32:11 +1300

    debugging

--- nightly_version.R
+++ nightly_version.R
@@ -1,5 +1,6 @@
 v <- strsplit(as.character(packageVersion("iNZight")), "\\.")[[1]]
-if (length(v) == 3L || v[4] == 9000) {
+print(v)
+if (length(v) == 3L || v[4] == "9000") {
     v[4] <- format(Sys.time(), "%Y%m%d", tz = "Pacific/Auckland")
 }
 cat(paste(v, collapse = "."))
